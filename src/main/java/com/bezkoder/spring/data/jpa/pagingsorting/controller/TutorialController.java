93b39b317e4181cbcd6556068ed5646855a247ff
Anand Varkey Philips <anandvarkeyphilips@gmail.com>
2020-12-01 15:07:53 +0530

    Cleaning up and adding db file

--- src/main/java/com/bezkoder/spring/data/jpa/pagingsorting/controller/TutorialController.java
+++ src/main/java/com/bezkoder/spring/data/jpa/pagingsorting/controller/TutorialController.java
@@ -42,38 +42,7 @@ public class TutorialController {
     } else if (direction.equals("desc")) {
       return Sort.Direction.DESC;
     }
-
     return Sort.Direction.ASC;
-  }
-
-  @GetMapping("/sortedtutorials")
-  public ResponseEntity<List<Tutorial>> getAllTutorials(@RequestParam(defaultValue = "id,desc") String[] sort) {
-
-    try {
-      List<Order> orders = new ArrayList<Order>();
-
-      if (sort[0].contains(",")) {
-        // will sort more than 2 fields
-        // sortOrder="field, direction"
-        for (String sortOrder : sort) {
-          String[] _sort = sortOrder.split(",");
-          orders.add(new Order(getSortDirection(_sort[1]), _sort[0]));
-        }
-      } else {
-        // sort=[field, direction]
-        orders.add(new Order(getSortDirection(sort[1]), sort[0]));
-      }
-
-      List<Tutorial> tutorials = tutorialRepository.findAll(Sort.by(orders));
-
-      if (tutorials.isEmpty()) {
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-      }
-
-      return new ResponseEntity<>(tutorials, HttpStatus.OK);
-    } catch (Exception e) {
-      return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
-    }
   }
 
   @GetMapping("/tutorials")
@@ -82,73 +51,38 @@ public class TutorialController {
       @RequestParam(defaultValue = "0") int page,
       @RequestParam(defaultValue = "3") int size,
       @RequestParam(defaultValue = "id,desc") String[] sort) {
-
     try {
       List<Order> orders = new ArrayList<Order>();
-
-      if (sort[0].contains(",")) {
-        // will sort more than 2 fields
-        // sortOrder="field,direction"
-        for (String sortOrder : sort) {
+      for (String sortOrder : sort) {
+        if (sort[0].contains(",")) {
           String[] _sort = sortOrder.split(",");
           orders.add(new Order(getSortDirection(_sort[1]), _sort[0]));
         }
-      } else {
-        // sort=[field,direction]
-        orders.add(new Order(getSortDirection(sort[1]), sort[0]));
       }
 
-      List<Tutorial> tutorials = new ArrayList<Tutorial>();
       Pageable pagingSort = PageRequest.of(page, size, Sort.by(orders));
-
       Page<Tutorial> pageTuts;
       if (title == null)
         pageTuts = tutorialRepository.findAll(pagingSort);
       else
         pageTuts = tutorialRepository.findByTitleContaining(title, pagingSort);
 
-      tutorials = pageTuts.getContent();
+      List<Tutorial> tutorials = pageTuts.getContent();
 
       Map<String, Object> response = new HashMap<>();
       response.put("tutorials", tutorials);
       response.put("currentPage", pageTuts.getNumber());
       response.put("totalItems", pageTuts.getTotalElements());
       response.put("totalPages", pageTuts.getTotalPages());
-
       return new ResponseEntity<>(response, HttpStatus.OK);
     } catch (Exception e) {
       return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
     }
   }
 
-  @GetMapping("/tutorials/published")
-  public ResponseEntity<Map<String, Object>> findByPublished(
-      @RequestParam(defaultValue = "0") int page,
-      @RequestParam(defaultValue = "3") int size) {
-    
-    try {
-      List<Tutorial> tutorials = new ArrayList<Tutorial>();
-      Pageable paging = PageRequest.of(page, size);
-
-      Page<Tutorial> pageTuts = tutorialRepository.findByPublished(true, paging);
-      tutorials = pageTuts.getContent();
-
-      Map<String, Object> response = new HashMap<>();
-      response.put("tutorials", tutorials);
-      response.put("currentPage", pageTuts.getNumber());
-      response.put("totalItems", pageTuts.getTotalElements());
-      response.put("totalPages", pageTuts.getTotalPages());
-
-      return new ResponseEntity<>(response, HttpStatus.OK);
-    } catch (Exception e) {
-      return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-    }
-  }
-
   @GetMapping("/tutorials/{id}")
   public ResponseEntity<Tutorial> getTutorialById(@PathVariable("id") long id) {
     Optional<Tutorial> tutorialData = tutorialRepository.findById(id);
-
     if (tutorialData.isPresent()) {
       return new ResponseEntity<>(tutorialData.get(), HttpStatus.OK);
     } else {
@@ -199,6 +133,5 @@ public class TutorialController {
     } catch (Exception e) {
       return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
     }
-
   }
 }
--- src/main/java/com/bezkoder/spring/data/jpa/pagingsorting/repository/TutorialRepository.java
+++ src/main/java/com/bezkoder/spring/data/jpa/pagingsorting/repository/TutorialRepository.java
@@ -10,9 +10,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.bezkoder.spring.data.jpa.pagingsorting.model.Tutorial;
 
 public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
-  Page<Tutorial> findByPublished(boolean published, Pageable pageable);
 
   Page<Tutorial> findByTitleContaining(String title, Pageable pageable);
-  
-  List<Tutorial> findByTitleContaining(String title, Sort sort);
 }
